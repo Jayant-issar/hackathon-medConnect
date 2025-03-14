7b6a96d24ed31db464c92dca185e22bd14907c64
JAYANT ISSAR <jayantissar23@gmail.com>
2025-03-14 20:07:07 +0530

    added action function to call getAllBloodBanks API

--- src/actions/bloodbank/index.ts
+++ src/actions/bloodbank/index.ts
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_API_URL
 export async function getBloodBanks() {
     
     try {
-        const response = await axios.get(`${API_URL}/bloodbank/all`)
+        const response = await axios.get(`${API_URL}/bloodbanks`)
         if (response.status === 200) {
             return response.data
         }
