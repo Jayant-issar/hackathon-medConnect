59df96df28a31a8af4510ae39e66d4c092cfc6c1
JAYANT ISSAR <jayantissar23@gmail.com>
2025-03-14 20:23:02 +0530

    added about the developer (jayant issar)

---
+++ src/data/constants.ts
@@ -0,0 +1,13 @@
+export const teamMembers = [
+    {
+      name: "Jayant Issar",
+      role: "FullStack Developer",
+      image: "https://media.licdn.com/dms/image/v2/D5603AQGP6TSBmBmtdA/profile-displayphoto-shrink_400_400/B56ZSivlQhGsAg-/0/1737897171516?e=1747267200&v=beta&t=Cy_TrXbAior3TgIajyf37qlWEN6gBH5tmzOO525zy3w",
+      socials: {
+        github: "https://github.com/Jayant-issar",
+        linkedin: "https://www.linkedin.com/in/jayant-issar/",
+        email: "mailto:Jayantissar23@gmail.com"
+      }
+    },
+   
+  ];
