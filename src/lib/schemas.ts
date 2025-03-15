9e07b495d2476b14cf0ae90057d34e5339124e76
JAYANT ISSAR <jayantissar23@gmail.com>
2025-03-15 19:00:39 +0530

    added create emergency schema

---
+++ src/lib/schemas.ts
@@ -0,0 +1,11 @@
+import {z} from "zod"
+export const createEmergencySchema = z.object({
+    name: z.string().min(3, { message: "Name must be at least 3 characters long" }),
+    location: z.string().min(3, { message: "Location must be specified" }),
+    contactName: z.string().min(3, { message: "Contact name must be provided" }),
+    contactPhone: z.string().min(10, { message: "Contact phone must be valid" }), 
+    urgencyLevel: z.string(),
+    description: z.string().optional(),
+    hospitalName: z.string().optional(),
+  });
+
